268407045bb417bf4f04589b0238d876bb85a183
sarthakgoel002 <sarthak.goel002@gmail.com>
2025-08-23 17:03:08 +0530

    bumped react vesrion

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Services from "./Services";
 import Contact from "./Contact";
 import "../App.css";
